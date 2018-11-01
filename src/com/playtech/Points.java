5f6f70b04340e2da6c15acb2f3dd76cb77d02b34
Daniil Romazanov <daniil.romazanov@playtech.com>
2018-11-01 13:53:47 +0200

    no-idea

--- src/com/playtech/Points.java
+++ src/com/playtech/Points.java
@@ -1,8 +1,6 @@
 package com.playtech;
 
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,15 +9,35 @@ import java.util.List;
 public class Points {
 
     public static void main(String[] args) throws IOException {
-        Rectangle rectangle = new Rectangle(5, 5, 10, 5);
-        Shape circle = new Ellipse2D.Double(0, 0, 100, 100);
+        Circle circle = new Circle(0, 0, 20);
+
         List<String> points = Files.readAllLines(Paths.get("points.txt"));
 
         points
                 .stream()
                 .map(s -> s.split(";"))
-                .map(strarr -> new Point2D.Double(Double.valueOf(strarr[0]), Double.valueOf(strarr[1])))
-                .filter()
+                .map(strarr -> new Point(Integer.valueOf(strarr[0]), Integer.valueOf(strarr[1])))
+                .filter(circle::contains)
                 .forEach(System.out::println);
+    }
+
+    private static class Circle {
+        private int x;
+        private int y;
+        private int radius;
+
+        private Circle(int x, int y, int radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        private boolean contains(Point point) {
+            return radius > distance(point);
+        }
+
+        private double distance(Point b) {
+            return Math.sqrt(Math.pow(this.x - b.x, 2) + Math.pow(this.y - b.y, 2));
+        }
     }
 }
